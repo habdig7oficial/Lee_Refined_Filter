ff323eecd0928bd6eb8c10cb99869081f7c7b4cc
Mateus Felipe da Silveira Vieira <habdig7oficial@MacBook-Pro-von-Mateus.local>
2026-07-09 15:58:41 -0300

    save

--- src/tests/config.hpp
+++ src/tests/config.hpp
@@ -1,8 +1,8 @@
-//#include "catch2/catch_test_macros.hpp"
-//#include "catch2/catch_get_random_seed.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_get_random_seed.hpp"
 #include "random"
+#include "math.h"
 
 #define TOLERANCE 1e-14
 #define REPETITION 10
 
-//uint32_t seed = Catch::getSeed();
