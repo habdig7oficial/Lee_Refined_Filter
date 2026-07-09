aae8b32e5aa68903cda9fa16083024ad4ebeabf7
Mateus Felipe da Silveira Vieira <habdig7oficial@MacBook-Pro-von-Mateus.local>
2026-07-09 15:58:41 -0300

    save

--- src/tests/gierull/gierull_test.hpp
+++ src/tests/gierull/gierull_test.hpp
@@ -12,16 +12,20 @@ TEST_CASE("Gierull point", "[point]"){
   
   RInside R(1, argv);
 
+  mt19937 rng(Catch::getSeed());
+  uniform_real_distribution<double> distribution(-numbers::pi, numbers::pi);
+
   for(int i = 0; i < REPETITION; i++){
     /* === Rand Params === */
-    //mt19937 rng(seed);
-    
-    double point = 0.5;
+
+    double point = distribution(rng);
     Param cpp_param = {
       .r = 0,
       .theta = 0.7,
       .L = 16
     };
+
+    cout << "Point = " << point << endl;
     
     /* === R TESTS === */ 
     R.parseEvalQ("source(\"src/tests/gierull/gierull_base.R\")");
@@ -42,7 +46,7 @@ TEST_CASE("Gierull point", "[point]"){
 
     cout << "C++ Result: " << cpp_res << endl;
 
-    cout << rand() << endl;
+    cout << endl;
   
     REQUIRE(abs(cpp_res - r_res) < TOLERANCE);
   }
