cf6fa74d671c7a96200381b4ae17ee56b5194fe1
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-10 10:53:08 -0300

    integrate ok

--- src/tests/config.hpp
+++ src/tests/config.hpp
@@ -2,7 +2,8 @@
 #include "catch2/catch_get_random_seed.hpp"
 #include "random"
 #include "math.h"
+#include "../lib/filter.hpp"
 
-#define TOLERANCE 1e-14
+#define EPSILON 1e-14
 #define REPETITION 10
 
