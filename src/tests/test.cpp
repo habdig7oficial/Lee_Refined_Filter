a7f8a9ef1f2083a2e00ab0f1d536a817ab33d4d4
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-10 13:10:19 -0300

    Extern R ptr

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,4 +1,6 @@
 #include "iostream"
+#include "catch2/catch_session.hpp"
+#include "config.hpp"
 
 #include "Rcpp.h"
 #include "RInside.h"
@@ -6,5 +8,15 @@
 /* Tests */
 #include "gierull/gierull_test.hpp"
 
+unique_ptr<RInside> Rbind;
 
+int main( int argc, char* argv[] ) {
 
+    cout << fixed << setprecision(15); 
+
+    Rbind = make_unique<RInside>(argc, argv);
+
+    int result = Catch::Session().run( argc, argv );
+
+    return result;
+}
