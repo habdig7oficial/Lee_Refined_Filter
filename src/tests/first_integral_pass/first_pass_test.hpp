4516c5ac2b01396deafd97d90e090eb022705c3c
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-12 14:54:11 -0300

    ok

---
+++ src/tests/first_integral_pass/first_pass_test.hpp
@@ -0,0 +1,3 @@
+TEST_CASE("First integral pass", "[first_pass]"){
+    (*Rbind).parseEvalQ("source(\"src/tests/first_integral_pass/filtragem_img_simulada.R\")");
+}
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -7,6 +7,7 @@
 
 /* Tests */
 #include "gierull/gierull_test.hpp"
+#include "first_integral_pass/first_pass_test.hpp"
 
 unique_ptr<RInside> Rbind;
 
@@ -15,6 +16,8 @@ int main( int argc, char* argv[] ) {
     cout << fixed << setprecision(15); 
 
     Rbind = make_unique<RInside>(argc, argv);
+
+    //(*Rbind).parseEvalQ("source(\"src/tests/install_libs.R\")");
 
     int result = Catch::Session().run( argc, argv );
 
