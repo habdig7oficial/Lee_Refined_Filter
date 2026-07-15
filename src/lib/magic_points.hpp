1a8efa5c14ffc42d227e3618bf74cbe15a63c320
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-15 19:48:08 -0300

    window 1

--- src/lib/magic_points.hpp
+++ src/lib/magic_points.hpp
@@ -121,6 +121,7 @@ MagicPoints window0(
 
 MagicPoints window1(
     vector<Point>{
+        {1, -1}, {0, 1},
         {1, 1}, {1, 0},
         {2, 2}, {2, 1}, {2, 0},
         {3, 2}, {3, 1}, {3, 0},
@@ -128,7 +129,7 @@ MagicPoints window1(
         {5, 2}, {5, 1}, {5, 0}, 
     }, 
     vector<Point>{
-        {0, 1}, {0, 0}
+        {0, 0}
     }
 );
 
