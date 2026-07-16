2a560d7aeebb7f980df687a4ddb54b38c7a8eb15
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-15 21:24:28 -0300

    window 5

--- src/lib/magic_points.hpp
+++ src/lib/magic_points.hpp
@@ -175,6 +175,20 @@ MagicPoints window4(
     }
 );
 
+MagicPoints window5(
+    vector<Point>{
+        {0, 1},
+        {1, 1}, {1, 0}, {1, 2}, 
+        {2, 3}, {2, 2}, {2, 1},
+        {3, 4}, {3, 3}, {3, 2},
+        {4, 5}, {4, 4}, {4, 3},
+        {5, 5}, {5, 4}, 
+
+    }, 
+    vector<Point>{
+        {0, 0}
+    }
+);
 
 
 vector<MagicPoints> all_windows = {window0, window1, window2};
--- src/tests/magic_points/magic_points_test.hpp
+++ src/tests/magic_points/magic_points_test.hpp
@@ -1,7 +1,7 @@
 TEST_CASE("First integral pass", "[magic_points]"){
     (*Rbind).parseEvalQ("plot(0,0, xlim = c(-5, 5), ylim = c(-5, 5))");
     
-    window4.traverse([](char rx, char ry, bool scope){
+    window5.traverse([](char rx, char ry, bool scope){
         if(scope == SIDE_A || true){
             (*Rbind)["rx"] = (int)rx;
             (*Rbind)["ry"] = (int)ry;
