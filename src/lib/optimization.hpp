11f2b5ba8ced91fa29fc5b7b44f395cbf22cd62b
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-17 11:33:46 -0300

    run

--- src/lib/optimization.hpp
+++ src/lib/optimization.hpp
@@ -4,4 +4,5 @@
 template <typename T, typename Lambda, size_t N>
 constexpr array<T, N> sort_compile_time(array<T, N> arr, Lambda lambda){
     sort(arr.begin(), arr.end(), lambda);
+    return arr;
 }
