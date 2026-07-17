4ca7636b153b791e4e9e2e67bf1dffe4498b1866
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-17 18:30:44 -0300

    calc mean comple

--- src/tests/calc_mean_complex/calc_mean_complex_test.hpp
+++ src/tests/calc_mean_complex/calc_mean_complex_test.hpp
@@ -7,10 +7,9 @@ TEST_CASE("Mean Complex win0", "[mean_complex]"){
     std::fill(arr.begin(), arr.end(), 5);
     cv::Mat arr_mat(arr, false);
 
-    calc_mean_complex<float>(arr_mat, 5, 5, window0);
-
     Mat test1 = arr_mat.reshape(0, 11);
 
+    calc_mean_complex<float>(test1, 5, 5, window0, true);
 
     (*Rbind)["matrix"] = arr;
     (*Rbind).parseEvalQ("matrix <- dim(matrix) <- c(11,11)");
