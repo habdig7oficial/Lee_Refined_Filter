5bcc4521b8987f7292591e6f202f8caf157c1260
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-07-19 17:57:48 -0300

    array is the same

--- src/tests/smooth_angles/smooth_angles.hpp
+++ src/tests/smooth_angles/smooth_angles.hpp
@@ -12,3 +12,22 @@ TEST_CASE("First integral pass", "[smooth_angles]"){
         (*Rbind).parseEvalQ("i <- i + 1");
    }
 }
+
+TEST_CASE("Vector test", "[sm_vec]"){
+  constexpr size_t len = DIMENSION + 2;
+  array<double, len> distance = dist<len>();
+
+  (*Rbind).parseEvalQ("w <- 1 / sqrt((1:13 - 6)^2 + (1:13 - 6)^2)");
+  (*Rbind).parseEvalQ("w[6] <- 0  # Delete masked pixels");
+
+
+  (*Rbind).parseEvalQ("i <- 1");
+  for(int i = 0; i < len; i++){
+    double r_res = (*Rbind).parseEval("w[[i]]");
+
+    REQUIRE(abs(distance[i] - r_res) < EPSILON);
+
+    (*Rbind).parseEvalQ("i <- i + 1");
+  }
+
+}
