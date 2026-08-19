e19abe5e6979b09506fb17a39eb2b76971411554
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-08-18 23:22:35 -0300

    ok

--- src/lib/point.hpp
+++ src/lib/point.hpp
@@ -16,6 +16,17 @@ inline constexpr size_t size_point = (dimension / 2 <= (SCHAR_MAX >> (CHAR_BIT /
 #define NOT_ROTATED true
 #define ROTATED false
 
+<<<<<<< HEAD
+=======
+inline constexpr size_t dimension =  11;
+inline constexpr size_t dimension_inner = 3;
+
+inline constexpr size_t inner_half = dimension_inner / 2;
+inline constexpr size_t inner_area = (dimension_inner * dimension_inner);
+//#define RELEVANT_POINTS_SIZE(N) (2 * INNER_AREA * (N + 1)) 
+
+inline constexpr size_t size_point = (dimension / 2 <= (SCHAR_MAX >> (CHAR_BIT / 2)))? CHAR_BIT / 2 : CHAR_BIT;
+>>>>>>> 9dd2330 (tidy up)
 
 /* In side A NOT_ROTATED is (x, y) */
 namespace Magic {
