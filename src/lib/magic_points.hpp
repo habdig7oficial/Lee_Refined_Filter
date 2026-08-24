63f1affcedc9615e573b0e7c54c3faddc806e063
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-08-23 21:11:31 -0300

    ok

--- src/lib/magic_points.hpp
+++ src/lib/magic_points.hpp
@@ -260,12 +260,12 @@ constexpr array<T, N> all_angles(){
 };
 
 constexpr size_t masks_size = MagicPoints::num_windows(dimension) / 2;
-constexpr array<MagicPoints, masks_size> magic_points_arr(){
-  array<MagicPoints, masks_size> masks = {};
+constexpr array<BitSetMask<dimension>, masks_size> magic_points_arr(){
+  array<BitSetMask<dimension>, masks_size> masks;
 
   for(int i = 0; i < masks_size; i++){
-    //masks[i] = MagicPoints::gen_mask<dimension>(MagicPoints::angle(i, dimension), thickness);
-    masks[i] = MagicPoints(i);
+    masks[i] = MagicPoints::gen_mask<dimension>(MagicPoints::angle(i, dimension), thickness);
+    //masks[i] = MagicPoints(i);
   }
 
   return masks;
